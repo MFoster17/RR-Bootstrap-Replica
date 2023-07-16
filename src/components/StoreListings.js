6fc6d2b7c00a459e46ec278b236dda4d1bd6b6ba
MFoster17 <ausslanderofficial@gmail.com>
2023-07-15 23:02:07 -0700

    fixed all errors and background image css

--- src/components/StoreListings.js
+++ src/components/StoreListings.js
@@ -3,8 +3,6 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-
-
 export default function StoreListings() {
     return (
         <div style={{ margin: "auto" }}>
